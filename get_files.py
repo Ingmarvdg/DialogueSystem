1ec7711e12c271b1f63236110cb77b848fb8bec9
Arccos390 <32079100+Arccos390@users.noreply.github.com>
2019-09-10 19:56:42 +0300

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -1,10 +1,6 @@
 import os
 
-json_log = "data/Mar13_S2A0/voip-00d76b791d-20130327_005342/log.json"
-json_label = "data/Mar13_S2A0/voip-00d76b791d-20130327_005342/label.json"
-
 entries = os.listdir('data/')
-print(entries)
 for parent_directories in entries:
 	entries_child = os.listdir('data/' + parent_directories + '/')
 	for child_directories in entries_child:
