a1d83f1ddf750e198138d4ac974154f378993597
Markos <polomarko390@gmail.com>
2019-09-23 04:18:46 +0300

    Adding .gitignore file

--- get_files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-entries = os.listdir('data/')
-for parent_directories in entries:
-	entries_child = os.listdir('data/' + parent_directories + '/')
-	for child_directories in entries_child:
-		log_file = 'data/' + parent_directories + '/' + child_directories + '/log.json'
-		label_file = 'data/' + parent_directories + '/' + child_directories + '/label.json'
-		## Grabs data from data folder. Needs a loop
